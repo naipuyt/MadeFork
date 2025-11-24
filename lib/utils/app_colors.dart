e0d8381fec19317ca5f780e0c695ef9ab2ab7f87
naiput <uptowngirl@naiput.local>
2025-11-24 10:26:24 +0700

    adding app color

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFF4B3A); // Orange-Red
+  static const Color background = Color(0xFFF2F2F2); // Light Grey
+  static const Color textColor = Color(0xFF7B7B7D); // Medium Grey
+}
